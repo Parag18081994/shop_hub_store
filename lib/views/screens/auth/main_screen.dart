1b3f41e5e69a1b1fcfd4b8e480e776624a48b898
Parag Naik <paragnaik2021.git@gmail.com>
2024-04-25 17:14:31 -0700

    Main_screen icons added

--- lib/views/screens/auth/main_screen.dart
+++ lib/views/screens/auth/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -11,16 +12,33 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          icon: Image.asset('assets/icons/store-1.png'),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('assets/icons/store-1.png'),
-          label: 'Home',
-        )
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/store-1.png',
+              width: 20,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/images/explore.svg'),
+            label: 'Catergories',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/images/cart.svg'),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/images/favorite.svg'),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/images/account.svg'),
+            label: 'account',
+          ),
+        ],
+      ),
     );
   }
 }
